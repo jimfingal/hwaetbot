642d5afd27c57bc1108b225c58aa013c40e1c002
Jim Fingal <jim.fingal@gmail.com>
2014-12-01 23:49:56 -0800

    scraper pipeline

--- riddlescrape/riddlescrape/spiders/exeter.py
+++ riddlescrape/riddlescrape/spiders/exeter.py
@@ -1,15 +1,21 @@
 # -*- coding: utf-8 -*-
-import scrapy
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors import LinkExtractor
+from unipath import Path
 
-
-class ExeterSpider(scrapy.Spider):
+class ExeterSpider(CrawlSpider):
     name = "exeter"
-    allowed_domains = ["http://en.wikisource.org/wiki/Anglo-Saxon_Riddles_of_the_Exeter_Book"]
+    allowed_domains = ["en.wikisource.org"]
     start_urls = (
         'http://en.wikisource.org/wiki/Anglo-Saxon_Riddles_of_the_Exeter_Book',
     )
 
-    def parse(self, response):
-        filename = response.url.split("/")[-1] + '.html'
-        with open(filename, 'wb') as f:
-            f.write(response.body)
+    rules = (
+        # Extract links matching 'item.php' and parse them with the spider's method parse_item
+        Rule(LinkExtractor(allow=".*wiki/Anglo-Saxon_Riddles_of_the_Exeter_Book.*"), callback='save_file'),
+    )
+
+    def save_file(self, response):
+        self.log('Hi, this is an item page! %s' % response.url)
+        p = Path("data/" + response.url.split("/")[-1] + '.html')
+        p.write_file(response.body)
